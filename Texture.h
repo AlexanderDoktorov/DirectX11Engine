9afd4843b447c5f1a7e843b21981ce980f0173da
Doktorov Alexander <doktorov.alexander.01@mail.ru>
2023-09-08 15:54:45 +0300

    Texture is no more bindable

--- Texture.h
+++ Texture.h
@@ -2,19 +2,18 @@
 #include "IBindable.h"
 #include <wrl.h>
 
-class Texture : public IBindable
+class Texture
 {
 public:
 	Texture(Graphics& Gfx, DXGI_FORMAT textureFormat, UINT TextureHeight, UINT TextureWidth);
 	Texture(Graphics& Gfx, const wchar_t* FileName);
 
-	void					CreateRTVTexture(Graphics& Gfx, DXGI_FORMAT textureFormat, UINT TextureHeight, UINT TextureWidth);
-	void					Reset();
-	void					Resize(Graphics& Gfx, UINT TextureHeight, UINT TextureWidth);
-	ID3D11Texture2D*		GetTexture() const noexcept;
+	void							CreateRTVTexture(Graphics& Gfx, DXGI_FORMAT textureFormat, UINT TextureHeight, UINT TextureWidth);
+	void							Reset();
+	void							Resize(Graphics& Gfx, UINT TextureHeight, UINT TextureWidth);
+	ID3D11Texture2D*				GetTexture() const noexcept;
 	ID3D11ShaderResourceView*		GetSRV() const noexcept;
-	D3D11_TEXTURE2D_DESC	GetDesc() const noexcept;
-	virtual void			Bind(Graphics& Gfx) noexcept override;
+	D3D11_TEXTURE2D_DESC			GetDesc() const noexcept;	
 
 	~Texture();
 
