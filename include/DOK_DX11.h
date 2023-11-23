93120f6e812a05e6ebe8a09defdc6055dcbb8d50
Doktorov Alexander <doktorov.alexander.01@mail.ru>
2023-11-23 03:10:15 +0300

    Made operator== and operator!= for D3D11_BLEND_DESC's (not sure they are right)

--- include/DOK_DX11.h
+++ include/DOK_DX11.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <DirectXMath.h>
+#include <d3d11.h>
 
 DirectX::XMMATRIX DOK_XMMatrixTranslation(const DirectX::XMFLOAT3& translations);
 constexpr DirectX::XMFLOAT3 operator-(DirectX::XMFLOAT3 lhs, DirectX::XMFLOAT3 rhs) noexcept
 {
 	return DirectX::XMFLOAT3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
 }
+
+bool operator==(const D3D11_BLEND_DESC& lhs, const D3D11_BLEND_DESC& rhs) noexcept;
+bool operator!=(const D3D11_BLEND_DESC& lhs, const D3D11_BLEND_DESC& rhs) noexcept;
