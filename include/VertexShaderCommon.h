6b00a3546359049eb29f31bbff30efbc98c85aba
Doktorov Alexander <doktorov.alexander.01@mail.ru>
2023-12-14 23:24:35 +0300

    VertexShaderCommon::Resolve

--- include/VertexShaderCommon.h
+++ include/VertexShaderCommon.h
@@ -2,8 +2,9 @@
 #include "IShader.h"
 #include "Blob.h"
 #include "DOK_assert.h"
+#include "BindableSystem.h"
 
-class VertexShaderCommon : virtual public IShader
+class VertexShaderCommon : public IShader
 {
 public:
 	VertexShaderCommon() = default;
@@ -13,6 +14,8 @@ public:
 	void SetShaderResourses(Graphics& Gfx, UINT start_slot, std::vector<ID3D11ShaderResourceView*> srvs) noexcept override;
 	void SetConstantBuffers(Graphics& Gfx, UINT start_slot, std::vector<ID3D11Buffer*> constant_buffers) noexcept override;
 
+	static std::shared_ptr<VertexShaderCommon> Resolve(Graphics& Gfx, const wchar_t* FileName) noexcept;
+	static std::string  GenerateID(const wchar_t* FileName) noexcept;
 	virtual void		Unbind(Graphics& Gfx) noexcept override;
 	virtual void		Bind(Graphics& Gfx) noexcept override;
 	ID3DBlob*			GetBlob()			noexcept override;
--- src/VertexShaderCommon.cpp
+++ src/VertexShaderCommon.cpp
@@ -1,24 +1,31 @@
 #include "VertexShaderCommon.h"
+#include <comdef.h> 
 
 VertexShaderCommon::VertexShaderCommon(Graphics& Gfx, const wchar_t* FileName) : blob(FileName)
 {
 	CHECK_HR ( GetDevice(Gfx)->CreateVertexShader(blob.GetBlob()->GetBufferPointer(), blob.GetBlob()->GetBufferSize(), nullptr, &p_VertexShader) );
 }
-
 void VertexShaderCommon::LoadFromFile(Graphics& Gfx, const wchar_t* FileName)
 {
 	blob.ReadFile(FileName);
 	CHECK_HR ( GetDevice(Gfx)->CreateVertexShader(blob.GetBlob()->GetBufferPointer(), blob.GetBlob()->GetBufferSize(), nullptr, &p_VertexShader) );
 }
-
 void VertexShaderCommon::SetShaderResourses(Graphics& Gfx, UINT start_slot, std::vector<ID3D11ShaderResourceView*> srvs) noexcept
 {
 	GetContext(Gfx)->VSSetShaderResources(start_slot, (UINT)srvs.size(), srvs.data());
 }
-
 void VertexShaderCommon::SetConstantBuffers(Graphics& Gfx, UINT start_slot, std::vector<ID3D11Buffer*> constant_buffers) noexcept
 {
 	GetContext(Gfx)->VSSetConstantBuffers(start_slot, (UINT)constant_buffers.size(), constant_buffers.data());
+}
+std::shared_ptr<VertexShaderCommon> VertexShaderCommon::Resolve(Graphics& Gfx, const wchar_t* FileName) noexcept
+{
+	return BindableSystem::Resolve<VertexShaderCommon>(Gfx, FileName);
+}
+std::string VertexShaderCommon::GenerateID(const wchar_t* FileName) noexcept
+{
+	_bstr_t asciString(FileName);
+	return std::string(typeid(VertexShaderCommon).name()).append("%").append(asciString);
 }
 
 void VertexShaderCommon::Unbind(Graphics& Gfx) noexcept
